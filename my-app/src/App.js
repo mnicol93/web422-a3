207d696ec5415b2ce15c5d194a4699be65b4a809
Marc Nicolas Oliva <mnicolas-oliva@myseneca.ca>
2021-10-14 22:09:48 -0400

    my-app added.

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import { Button, Col, Container, Form, FormControl, Nav, Navbar, Row } from 'react-bootstrap';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import React, { useEffect, useState } from "react";
+
+import './App.css';
+
+function App() {
+  const [ searchString, setSearchString ] = useState("");
+  
+  handleSubmit((e)=>{
+    
+  })
+  return (
+    null
+  );
+}
+
+export default App;
